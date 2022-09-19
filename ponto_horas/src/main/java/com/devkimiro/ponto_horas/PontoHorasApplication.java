3bf0d4c3d7c1f7802f48b97445f0ac83e228a01e
torh <torh@gft.com>
2022-09-19 11:30:12 -0300

    Inicializando o projeto

---
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/PontoHorasApplication.java
@@ -0,0 +1,13 @@
+package com.devkimiro.ponto_horas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoHorasApplication.class, args);
+	}
+
+}
---
+++ ponto_horas/src/test/java/com/devkimiro/ponto_horas/PontoHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkimiro.ponto_horas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
