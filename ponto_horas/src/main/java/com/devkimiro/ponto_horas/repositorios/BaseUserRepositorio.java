cbed1c80aa7aeeaab689bd37bc8ae216d8918749
torh <torh@gft.com>
2022-09-20 11:33:53 -0300

    Adicionando o repositório de base user

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/repositorios/BaseUserRepositorio.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/repositorios/BaseUserRepositorio.java
@@ -1,6 +1,9 @@
 package com.devkimiro.ponto_horas.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devkimiro.ponto_horas.entidades.BaseUser;
@@ -8,4 +11,9 @@ import com.devkimiro.ponto_horas.entidades.BaseUser;
 @Repository
 public interface BaseUserRepositorio extends JpaRepository<BaseUser, Long> {
     
+    @Query(value = "SELECT u FROM BaseUser u WHERE nome = :nome")
+    Optional<BaseUser> findByNome(String nome);
+
+    @Query(value = "SELECT u FROM BaseUser u WHERE email = :email")
+    Optional<BaseUser> findByEmail(String email);
 }
