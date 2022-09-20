d7626b1f5aa2930ff94e5aba101cc24e01be4e4f
torh <torh@gft.com>
2022-09-20 11:33:02 -0300

    Adicionando cargo como entidade

---
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/entidades/Cargo.java
@@ -0,0 +1,45 @@
+package com.devkimiro.ponto_horas.entidades;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_cargo")
+    @NotBlank(message = "O nome do cargo não pode ser enviado em branco!")
+    @NotNull(message = "O nome do cargo não pode ser vazio!")
+    private String nomeCargo;
+
+    @NotBlank(message = "O salário do cargo não pode ser enviado em branco!")
+    @NotNull(message = "O salário do cargo não pode ser vazio!")
+    private double salario;
+
+    @Column(name = "carga_horaria")
+    @JsonFormat(pattern = "yyyy-MM-DD HH-mm-ss", shape = Shape.STRING)
+    private LocalDateTime cargaHoraria;
+    
+}
