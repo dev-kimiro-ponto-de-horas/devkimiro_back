7cba8e2ddf20ac537ad4a576f9f3036f831de1c3
torh <torh@gft.com>
2022-09-20 11:36:25 -0300

    Adicionando serviço de base user

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/servicos/BaseUserServico.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/servicos/BaseUserServico.java
@@ -1,13 +1,26 @@
 package com.devkimiro.ponto_horas.servicos;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devkimiro.ponto_horas.entidades.BaseUser;
 import com.devkimiro.ponto_horas.repositorios.BaseUserRepositorio;
 
 @Service
 public class BaseUserServico {
-    
+
     @Autowired
     private BaseUserRepositorio baseUserRepositorio;
+
+    public BaseUser buscarUserPorNome(String nome){
+        Optional<BaseUser> user = baseUserRepositorio.findByNome(nome);
+        return user.get();
+    }
+
+    public BaseUser buscarUserPorEmail(String email){
+        Optional<BaseUser> user = baseUserRepositorio.findByEmail(email);
+        return user.get();
+    }
 }
