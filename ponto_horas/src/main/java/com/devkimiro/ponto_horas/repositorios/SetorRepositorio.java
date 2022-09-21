c794809ec74e0e095b5ce502882bf9764368d97b
torh <torh@gft.com>
2022-09-21 16:10:13 -0300

    Adicionando query de buscar pelo nome do setor

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/repositorios/SetorRepositorio.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/repositorios/SetorRepositorio.java
@@ -1,11 +1,17 @@
 package com.devkimiro.ponto_horas.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devkimiro.ponto_horas.entidades.Setor;
 
 @Repository
 public interface SetorRepositorio extends JpaRepository<Setor, Long> {
+
+    @Query(value = "SELECT u FROM Setor u WHERE nomeSetor = :nomeSetor")
+    Optional<Setor> findByNomeSetor(String nomeSetor);
     
 }
