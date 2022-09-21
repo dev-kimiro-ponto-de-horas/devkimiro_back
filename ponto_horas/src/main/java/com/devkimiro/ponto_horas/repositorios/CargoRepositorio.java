bb0ea2f3546da2725c9acfdb7781ef07ecc4b42a
torh <torh@gft.com>
2022-09-21 11:17:20 -0300

    Atualizando o repositório de cargo

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/repositorios/CargoRepositorio.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/repositorios/CargoRepositorio.java
@@ -1,6 +1,9 @@
 package com.devkimiro.ponto_horas.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devkimiro.ponto_horas.entidades.Cargo;
@@ -8,4 +11,7 @@ import com.devkimiro.ponto_horas.entidades.Cargo;
 @Repository
 public interface CargoRepositorio extends JpaRepository<Cargo, Long> {
     
+    @Query(value = "SELECT u FROM Cargo u WHERE nomeCargo = :nomeCargo")
+    Optional<Cargo> findByNomeCargo(String nomeCargo);
+
 }
