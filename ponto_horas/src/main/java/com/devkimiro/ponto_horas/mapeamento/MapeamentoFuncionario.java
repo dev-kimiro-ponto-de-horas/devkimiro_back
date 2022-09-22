3bd00cdaccc6f4acf5327959e70c3fa3fc1184bb
torh <torh@gft.com>
2022-09-22 16:53:30 -0300

    Adicionando mapeamento de funcionário response

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/mapeamento/MapeamentoFuncionario.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/mapeamento/MapeamentoFuncionario.java
@@ -1,16 +1,14 @@
 package com.devkimiro.ponto_horas.mapeamento;
 
+import org.springframework.stereotype.Service;
+
 import com.devkimiro.ponto_horas.dto.response.FuncionarioResponseDto;
 import com.devkimiro.ponto_horas.entidades.Funcionario;
 
+@Service
 public class MapeamentoFuncionario {
-    
-    //de requestDTO para Funcionario
 
-    // de funcionario para responseDTO
-
-    public static FuncionarioResponseDto DeFuncionarioParaResponse (Funcionario funcionario){
+    public static  FuncionarioResponseDto deFuncionarioParaResponse (Funcionario funcionario){
         return new FuncionarioResponseDto(funcionario.getNome(), funcionario.getEmail(), funcionario.getCargo().getNomeCargo());
     }
-
 }
