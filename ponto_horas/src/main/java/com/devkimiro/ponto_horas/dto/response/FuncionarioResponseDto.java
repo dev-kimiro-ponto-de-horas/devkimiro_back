1f23ca205861434dbaf4a337cd5decf1a7908ac3
torh <torh@gft.com>
2022-09-22 10:45:26 -0300

    Adicionando a response de funcionario ao sistema

---
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/dto/response/FuncionarioResponseDto.java
@@ -0,0 +1,18 @@
+package com.devkimiro.ponto_horas.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FuncionarioResponseDto {
+    
+    private String nome;
+    private String email;
+    private String nomeCargo;
+
+}
