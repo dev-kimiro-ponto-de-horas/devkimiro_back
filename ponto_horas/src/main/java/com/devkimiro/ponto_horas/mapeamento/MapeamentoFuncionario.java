589087a0cb31662b0e5238a2068e23bf9ffb775a
torh <torh@gft.com>
2022-09-28 16:28:59 -0300

    Removendo a service do mapeamento de funcionário

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/mapeamento/MapeamentoFuncionario.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/mapeamento/MapeamentoFuncionario.java
@@ -1,11 +1,8 @@
 package com.devkimiro.ponto_horas.mapeamento;
 
-import org.springframework.stereotype.Service;
-
 import com.devkimiro.ponto_horas.dto.response.FuncionarioResponseDto;
 import com.devkimiro.ponto_horas.entidades.Funcionario;
 
-@Service
 public class MapeamentoFuncionario {
 
     public static  FuncionarioResponseDto deFuncionarioParaResponse (Funcionario funcionario){
