fe46b2498be086194a3bdef2bf1d37c49bb23c52
Azkalum <renan.132@outlook.com>
2022-10-01 02:52:15 -0300

    Adicionado o PassworEncoder

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/PontoHorasApplication.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/PontoHorasApplication.java
@@ -2,12 +2,24 @@ package com.devkimiro.ponto_horas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PontoHorasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoHorasApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		return encoder;
 	}
 
 }
