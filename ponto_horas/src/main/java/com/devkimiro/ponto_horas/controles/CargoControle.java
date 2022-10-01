7a081e76c87bcc141ab4fb100303f912056e17f3
Azkalum <renan.132@outlook.com>
2022-10-01 02:06:28 -0300

    Adicionando o ApiResponse ao CargoControle

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/controles/CargoControle.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/controles/CargoControle.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,22 +21,32 @@ import com.devkimiro.ponto_horas.servicos.CargoServico;
 
 @RestController
 @RequestMapping("/cargo")
+@Api("Controle de Cargo")
 public class CargoControle {
 
     @Autowired
     private CargoServico cargoServico;
 
     @GetMapping
+    @ApiOperation("Listar todos os Cargos")
+    @ApiResponse(code = 200, message = "Todos os cargos listados com sucesso!")
     public ResponseEntity <List<Cargo>> listarTodosCargos(){
         return ResponseEntity.ok(cargoServico.listarTodosCargos());
     }
 
     @PostMapping
+    @ApiOperation("Criar um Cargo")
+    @ApiResponse(code = 200,message = "Cargo Criado com Sucesso!")
     public ResponseEntity<Cargo> criarCargo (@Valid @RequestBody Cargo cargo){
         return ResponseEntity.ok(cargoServico.criarCargo(cargo));
     }
 
     @GetMapping("{id}")
+    @ApiOperation("Buscar um Cargo por ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cargo encontrado com sucesso!") ,
+            @ApiResponse(code = 404, message = "Cargo não encontrado!")
+    })
     public ResponseEntity <Cargo> buscarCargoPorId (@PathVariable Long id){
         try{
         return ResponseEntity.ok(cargoServico.buscarCargoPorId(id));
@@ -45,6 +56,11 @@ public class CargoControle {
     }
 
     @GetMapping("/nome/{nomeCargo}")
+    @ApiOperation("Buscar Cargo por Nome")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cargo encontrado com sucesso!") ,
+            @ApiResponse(code = 404, message = "Cargo não encontrado!")
+    })
     public ResponseEntity <Cargo> buscarCargoPorNome (@PathVariable String nomeCargo){
         try{
         return ResponseEntity.ok(cargoServico.buscarCargoPorNome(nomeCargo));
@@ -54,6 +70,11 @@ public class CargoControle {
     }
 
     @PutMapping("{id}")
+    @ApiOperation("Atualizar Cargo")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cargo atualizado com sucesso!") ,
+            @ApiResponse(code = 404, message = "Cargo não encontrado!")
+    })
     public ResponseEntity<Cargo> atualizarCargo (@Valid @RequestBody Cargo cargo, @PathVariable Long id){
         try{
         return ResponseEntity.ok(cargoServico.atualizarCargo(cargo, id));
@@ -63,6 +84,11 @@ public class CargoControle {
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation("Deletar Cargo")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cargo deletado com sucesso!") ,
+            @ApiResponse(code = 404, message = "Cargo não encontrado!")
+    })
     public ResponseEntity<?> deletarCargo (@PathVariable Long id){
         try{
         cargoServico.deletarCargo(id);
