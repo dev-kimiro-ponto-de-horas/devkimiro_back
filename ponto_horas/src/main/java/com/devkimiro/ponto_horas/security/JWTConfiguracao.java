471019952530a656b1c77a79689b42367db2fdb2
Azkalum <renan.132@outlook.com>
2022-10-04 15:13:26 -0300

    Arrumando um pequeno erro de digitação

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/security/JWTConfiguracao.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/security/JWTConfiguracao.java
@@ -32,7 +32,7 @@ public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-R        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST,"/login").
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST,"/login").
                 permitAll().
                 anyRequest().
                 authenticated().
