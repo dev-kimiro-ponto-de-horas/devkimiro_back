0eeca2bb238b3788fecb94d4d1f06e4103d38fa2
Azkalum <renan.132@outlook.com>
2022-10-04 15:12:30 -0300

    Refatorando o código

--- ponto_horas/src/main/java/com/devkimiro/ponto_horas/security/JWTConfiguracao.java
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/security/JWTConfiguracao.java
@@ -32,14 +32,14 @@ public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST,"/login")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .addFilter(new JWTAutenticadorFilter(authenticationManager()))
-                .addFilter(new JWTValidarFilter(authenticationManager()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+R        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST,"/login").
+                permitAll().
+                anyRequest().
+                authenticated().
+                and().
+                addFilter(new JWTAutenticadorFilter(authenticationManager())).
+                addFilter(new JWTValidarFilter(authenticationManager())).
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
