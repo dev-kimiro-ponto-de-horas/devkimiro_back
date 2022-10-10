9a7d87f0dfb94207fac5303d4df1c87bd7d439f1
torh <torh@gft.com>
2022-10-10 14:26:37 -0300

    Adicionando a classe usuarioSistemaResponseDto

---
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/dto/response/UsuarioSistemaResponseDto.java
@@ -0,0 +1,19 @@
+package com.devkimiro.ponto_horas.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioSistemaResponseDto {
+    
+    private String nome;
+    private String email;
+    private String login;
+    private String senha;
+
+}
