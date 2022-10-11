74b41f56147310c5a714000e23e8ddd05f1be70a
torh <torh@gft.com>
2022-10-11 16:19:18 -0300

    Adicionando o repositório de calendário

---
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/repositorios/CalendarioRepositorio.java
@@ -0,0 +1,11 @@
+package com.devkimiro.ponto_horas.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devkimiro.ponto_horas.entidades.Calendario;
+
+@Repository
+public interface CalendarioRepositorio extends JpaRepository<Calendario, Long> {
+
+}
