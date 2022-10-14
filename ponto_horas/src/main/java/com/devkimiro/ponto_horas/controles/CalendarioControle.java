5120a7714d6498724fc333298f7ceee11949deba
torh <torh@gft.com>
2022-10-14 13:45:37 -0300

    Adicionando controller d/ calendario listarTodos

---
+++ ponto_horas/src/main/java/com/devkimiro/ponto_horas/controles/CalendarioControle.java
@@ -0,0 +1,25 @@
+package com.devkimiro.ponto_horas.controles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devkimiro.ponto_horas.entidades.Calendario;
+import com.devkimiro.ponto_horas.servicos.CalendarioServico;
+
+@RestController
+@RequestMapping("/calendario")
+public class CalendarioControle {
+    
+    @Autowired
+    private CalendarioServico calendarioServico;
+
+    @GetMapping("{cracha}")
+    public ResponseEntity<List<Calendario>> listarTodosCalendariosPorCracha (String cracha){
+        return ResponseEntity.ok(calendarioServico.buscarTodosCalendariosPorCracha(cracha));
+    }
+}
